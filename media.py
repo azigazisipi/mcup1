6f52b1f26f2bc33de92ca1e5e8f52225eb1e1224
azigazisipi <32300859+azigazisipi@users.noreply.github.com>
2018-03-01 10:05:49 +0100

    Add files via upload

--- media.py
+++ media.py
@@ -1,10 +1,26 @@
 import webbrowser
+
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """
+    Movie class determines the input and output parameters
+    """
+    
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        """
+        :param moviee_title: string
+        :param movie_storyline: string
+        :param poster image: string
+        :param trailer_youtube: string
+        """
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.stroyline = movie.stroyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         
     def show_trailer(self):
+        """
+        opens the movie's trailer from youtube
+        """
         webbrowser.open(self.trailer_youtube_url)

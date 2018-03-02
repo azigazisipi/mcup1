6d7409b9e334a1078feda6bae481827d33bdc79d
azigazisipi <32300859+azigazisipi@users.noreply.github.com>
2018-03-02 10:13:17 +0100

    Update media.py

--- media.py
+++ media.py
@@ -15,7 +15,7 @@ class Movie():
         :param trailer_youtube: string
         """
         self.title = movie_title
-        self.storyline = movie.storyline
+        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         
